e44282df78f051df454d11bdb83f1a37dcdbfe14
vallisneria <moogi3469@gmail.com>
2021-07-25 21:32:27 +0900

    setup 파일 url 추가

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ with open("requirements.txt", mode="r", encoding="utf-8") as deps_file:
 setuptools.setup(
     name="hate-speech-classification-model",
     version="0.0.1",
+    url="https://github.com/2021-hknu-cd-hate-speech-classification/model",
     description="한경대학교 컴퓨터공학과 캡스톤디자인",
     license="MIT",
     packages=setuptools.find_packages(),
